81e7c5af4966b3a6a67f565f367fbd19f1802c06
Raymond Heberer <ray.heberer@gmail.com>
2019-12-30 17:13:01 -0800

    Stabilize learning through using logits, raising epsilon, simplifying network

--- custom_models.py
+++ custom_models.py
@@ -2,6 +2,8 @@
 import numpy as np
 import tensorflow as tf
 
+from math import ceil
+
 
 def dense(inputs, weights, bias, activation=tf.identity, *activation_args):
     """Fully-connected layer."""
@@ -9,10 +11,11 @@ def dense(inputs, weights, bias, activation=tf.identity, *activation_args):
     return activation(x, *activation_args)
 
 
-def conv2d(inputs, filters, strides=1, padding="SAME", alpha=0.2):
+def conv2d(inputs, filters, strides=1, padding="SAME",
+           activation=tf.nn.relu, *activation_args):
     """Convolutional layer with 2D filter and ReLU activation."""
     x = tf.nn.conv2d(inputs, filters, strides, padding)
-    return tf.nn.leaky_relu(x, alpha=alpha)
+    return activation(x, *activation_args)
 
 
 def max_pool2d(inputs, ksize, strides=2, padding="SAME"):
@@ -21,12 +24,10 @@ def max_pool2d(inputs, ksize, strides=2, padding="SAME"):
     return x
 
 
-def init_weights(shape, name,
-                 initializer=tf.initializers.glorot_uniform()):
+def init_weights(shape, initializer=tf.initializers.glorot_uniform()):
     """Initialize weights for tensorflow layer."""
     weights = tf.Variable(
         initializer(shape),
-        name=name,
         trainable=True,
         dtype=tf.float32
     )
@@ -38,16 +39,17 @@ class SiameseNetwork(object):
     """Siamese CNN for facial recognition."""
 
     def __init__(self,
-                 input_dim=(243, 320),
-                 embed_dim=128,
+                 input_dim=(60, 60),
+                 embed_dim=1024,
                  optimizer=tf.optimizers.Adam,
-                 learning_rate=0.00005,
-                 checkpoint_dir=None):
+                 checkpoint_dir=None,
+                 **optimizer_kwargs):
         """Set hyperparameters, initialize weights."""
-        self.optimizer = optimizer(learning_rate)
+        self.input_dim = input_dim  # the input will be resized to this
+        self.optimizer = optimizer(**optimizer_kwargs)
 
         np.random.seed(41)
-        self.initialize_weights(input_dim, embed_dim)
+        self.initialize_weights(embed_dim)
 
         if checkpoint_dir is not None:
             assert checkpoint_dir[-1] == "/"
@@ -58,55 +60,64 @@ class SiameseNetwork(object):
                 **weights
             )
 
-    def initialize_weights(self, input_dim, embed_dim):
+    def initialize_weights(self, embed_dim):
         """Initialize weights."""
-        x = input_dim[0]
-        y = input_dim[1]
+        x = self.input_dim[0]
+        y = self.input_dim[1]
 
-        shapes = [
-            [5, 5, 1, 32],
-            [5, 5, 32, 32],
-            [5, 5, 32, 64],
-            [5, 5, 64, 64],
-            [64 * round(x / 4) * round(y / 4), embed_dim],
-            [1, embed_dim],
-            [embed_dim, 1],
-            [1, 1]
+        wshapes = [
+            [5, 5, 1, 128],
+            [3, 3, 128, 256],
+            [256 * ceil(x / 4) * ceil(y / 4), embed_dim],
+            [embed_dim, 1]
         ]
 
-        r = range(len(shapes))
-        weights = [init_weights(shapes[i], "weight{}".format(i)) for i in r]
+        bshapes = [
+            [1, embed_dim]
+        ]
+
+        rw = range(len(wshapes))
+        weights = [init_weights(wshapes[i]) for i in rw]
         self.weights = weights
+
+        rb = range(len(bshapes))
+        bias_initializer = tf.initializers.zeros()
+        biases = [init_weights(bshapes[i], bias_initializer) for i in rb]
+
+        self.biases = biases
 
     def embed(self, x):
         """Tensorflow model function."""
         x = tf.cast(x, dtype=tf.float32)
-        c1_1 = conv2d(x, self.weights[0])
-        c1_2 = conv2d(c1_1, self.weights[1])
-        p1 = max_pool2d(c1_2, 2)
+        x = tf.image.resize(x, self.input_dim)
 
-        c2_1 = conv2d(p1, self.weights[2])
-        c2_2 = conv2d(c2_1, self.weights[3])
-        p2 = max_pool2d(c2_2, 2)
+        c1 = conv2d(x, self.weights[0])
+        p1 = max_pool2d(c1, 2)
+
+        c2 = conv2d(p1, self.weights[1])
+        p2 = max_pool2d(c2, 2)
 
         flatten = tf.reshape(p2, shape=(tf.shape(p2)[0], -1))
-        embed = dense(flatten, self.weights[4], self.weights[5], tf.nn.sigmoid)
+        embed = dense(flatten, self.weights[2], self.biases[0], tf.nn.tanh)
         return embed
 
-    def similarity_score(self, x1, x2, embed=True):
+    def similarity_score(self, x1, x2, embed=True, logits=True):
         """Compute similarity score between -1 and 1."""
         embed1 = self.embed(x1) if embed else x1
         embed2 = self.embed(x2) if embed else x2
 
-        dist = tf.abs(embed2 - embed1)  # L2 norm causes instability near 0
+        dist = tf.abs(embed2 - embed1)
 
-        score = dense(dist, self.weights[6], self.weights[7], tf.nn.sigmoid)
+        score = dense(dist, self.weights[3], tf.constant([0.]))
 
-        return score
+        if logits:
+            return score
+        else:
+            return tf.nn.sigmoid(score)
 
     def loss(self, labels, preds):
         """Cross entropy loss."""
-        return tf.losses.binary_crossentropy(labels, preds)
+        return tf.losses.binary_crossentropy(labels, preds, True)
 
     def train_step(self, x1, x2, labels):
         """One train step on a batch of inputs and target outputs."""
@@ -114,8 +125,8 @@ class SiameseNetwork(object):
             preds = self.similarity_score(x1, x2)
             current_loss = self.loss(labels, preds)
 
-        grads = tape.gradient(current_loss, self.weights)
-        self.optimizer.apply_gradients(zip(grads, self.weights))
+        grads = tape.gradient(current_loss, self.weights + self.biases)
+        self.optimizer.apply_gradients(zip(grads, self.weights + self.biases))
         loss = tf.reduce_mean(current_loss)
 
         return grads, loss
